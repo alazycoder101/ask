4455999cb79635f5bb2ed7061988beccafd569cc
Joey <joeyw@reallyenglish.com>
2022-12-10 14:07:06 +0000

    add devise for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_for :users,
+             path: '', # optional namespace or empty string for no space
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               password: 'secret',
+               confirmation: 'verification',
+               registration: 'register',
+               sign_up: 'signup'
+             }
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds/001_user.rb
+++ db/seeds/001_user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,5 +7,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-Dir[Rails.root.join('db/seeds/*.rb')].each { |f| require f }
+password = ENV.fetch('TEST_PASSWORD', 'Test_1234')
+if Rails.env.development?
+  User.create!(email: 'user@ask.com', login: 'user1', name: 'John', password: password,
+               password_confirmation: password)
+end
