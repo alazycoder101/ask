1eb63051af63aded6c9d4bd6b52b8551e58051c4
Joey <joeyw@reallyenglish.com>
2022-12-11 17:42:14 +0000

    add answer and comment policy

---
+++ app/policies/comment_policy.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CommentPolicy < ApplicationPolicy
+  def show?
+    true
+  end
+
+  def update?
+    user && record.user_id == user.id
+  end
+
+  def create?
+    !user.nil?
+  end
+
+  def destroy?
+    update?
+  end
+end
