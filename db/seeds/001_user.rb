cda82bffa1c7b9f8f633d2f3efff5c45f67cf7d1
Joey <joeyw@reallyenglish.com>
2022-12-11 10:47:21 +0000

    add devise for authentication

---
+++ db/seeds/001_user.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+if Rails.env.development?
+  password = ENV.fetch('TEST_PASSWORD', 'Test_1234')
+  User.create!(email: 'user1@ask.com', login: 'user1', name: 'John', password: password,
+               password_confirmation: password)
+  User.create!(email: 'user2@ask.com', login: 'user2', name: 'Tom', password: password,
+               password_confirmation: password)
+  User.create!(email: 'user3@ask.com', login: 'user3', name: 'Jimmy', password: password,
+               password_confirmation: password)
+  User.create!(email: 'user4@ask.com', login: 'user4', name: 'Kate', password: password,
+               password_confirmation: password)
+end
+
