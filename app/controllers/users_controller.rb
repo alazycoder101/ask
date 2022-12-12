3559c40b1586156aa4ea6e2f99ce06a7105281b0
Joey <joeyw@reallyenglish.com>
2022-12-12 21:40:22 +0000

    follow unfollow support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit]
+  before_action :set_user, only: [:show, :edit, :toggle_follow, :follow, :unfollow]
 
-  def show
-  end
+  def show; end
 
   def index
     @users = User.order(:login).page params[:page]
+  end
+
+  def toggle_follow
+    if params[:val].to_i == 1
+      current_user.follow(@user)
+    else
+      current_user.unfollow(@user)
+    end
+
+    render plain: "OK"
   end
 
   def follow
--- app/javascript/controllers/toggle_action_controller.js
+++ app/javascript/controllers/toggle_action_controller.js
@@ -2,15 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "action" ]
-  static values = { labels: { type: Array, default: ['follow', 'unfollow'] }, toggle: { type: Number, default: 0 }}
+  static values = { url: String, labels: { type: Array, default: ['follow', 'unfollow'] }, toggle: { type: Number, default: 0 }}
 
   connect() {
     this.index = this.toggleValue
-    this.actionTarget.text = this.labelsValue[this.index % 2]
+    this.actionTarget.innerText = this.labelsValue[this.index % 2]
   }
 
   toggleAction() {
     this.index = (this.index + 1) % 2
-    this.actionTarget.innerText = this.labelsValue[this.index % 2]
+
+    var _this = this
+    fetch(this.urlValue + '?val='+ this.index)
+    .then(() => {
+       _this.actionTarget.innerText = _this.labelsValue[_this.index]
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   }
 }
