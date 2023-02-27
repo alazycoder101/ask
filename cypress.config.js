75207744fff5ac3244414579529ef41654b46777
lazycoder <alazycoder101@gmail.com>
2023-02-27 13:34:54 +0000

    use cypress action from github

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "bnoec6",
   e2e: {
     baseUrl: "http://localhost:5017",
     defaultCommandTimeout: 10000,
